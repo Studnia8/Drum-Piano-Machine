d723d20b425b663dd55fc3c8b7900fe288cef428
Studnia8 <michal.studnicki8@gmail.com>
2020-03-29 17:55:17 +0200

    Updates

---
+++ precache-manifest.a2eb938ce27dd081786b6db69ce8843d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b6258a95925f3c96a1b52322da59f2",
+    "url": "/drum-piano-machine/index.html"
+  },
+  {
+    "revision": "ace5c28ea329ee037843",
+    "url": "/drum-piano-machine/static/css/main.f4e9c327.chunk.css"
+  },
+  {
+    "revision": "b62452055fb6a9232a28",
+    "url": "/drum-piano-machine/static/js/2.bf52df6b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/drum-piano-machine/static/js/2.bf52df6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace5c28ea329ee037843",
+    "url": "/drum-piano-machine/static/js/main.4a2917a4.chunk.js"
+  },
+  {
+    "revision": "25f0e6792bf0d56c76da",
+    "url": "/drum-piano-machine/static/js/runtime-main.da1c186d.js"
+  }
+]);
